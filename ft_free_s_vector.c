3078e8ab5a250b66df50e8a4c74319a13d1bd428
coderMeira <fredericomeiraa@gmail.com>
2021-07-23 14:49:47 +0100

    added strcmp && free_s_vector

---
+++ ft_free_s_vector.c
@@ -0,0 +1,12 @@
+#include "../lib/libft.h"
+
+void	ft_free_s_vector(char **str, int size)
+{
+	int	i;
+
+	i = 0;
+	len = ft_strlen(original);
+	while (i < size)
+		free(str[i++])
+	return (true);
+}
