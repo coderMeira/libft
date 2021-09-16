6694534adce6123309b1d540847bc409c72b9710
coderMeira <fredericomeiraa@gmail.com>
2021-09-16 17:38:57 +0100

    headers applied && gnl with norm

--- src/free_slice.c
+++ src/free_slice.c
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free_slice.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fmeira <fmeira@student.42lisboa.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/16 17:30:05 by fmeira            #+#    #+#             */
+/*   Updated: 2021/09/16 17:32:13 by fmeira           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../inc/libft.h"
 
-void free_slice(char **arr, int current, int end)
+void	free_slice(char **arr, int current, int end)
 {
 	if (arr)
 	{
-		while (++current < end)
+		while (++current <= end)
 			free(arr[current]);
 	}
 }
